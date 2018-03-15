5f7ed9fbace0bd69140b074e296afd55221fdbf8
LEKPA <lekpamartin@yahoo.fr>
2018-03-15 10:12:03 +0100

    Add files via upload

---
+++ Configuration/cloud.deploy.d/post_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ENV=$1
+
+case $ENV in
+        ENV1)
+            PASSWORD="password"
+            GW=IP;;
+        ENV2)
+            PASSWORD="password"
+            GW=IP;;
+        *)
+			PASSWORD="password"
+			GW=IP;;
+esac
+
+# COMMON
+route add -net <DEST> netmask <MASK> gw ${GW}
+yum -y install salt-minion
+mkdir -p /etc/salt/pki
+echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
+echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
+
+cat /tmp/.*/grains > /etc/salt/grains
+cat /tmp/.*/minion > /etc/salt/minion
+
+/sbin/chkconfig salt-minion on
+service salt-minion start
+
+# Actions intéressantes en cas d'un besoin de reboot de la VM
+#echo "* * * * * root /bin/bash /tmp/tempo.sh > /dev/null 2>&1" > /etc/cron.d/tempo
+#echo "( sleep 61; echo ${PASSWORD} | passwd root --stdin; /sbin/reboot ) &
+#echo ${PASSWORD} | passwd root --stdin;
+#rm -f /tmp/tempo.sh; rm -f /tmp/tmp*; rm -f /etc/cron.d/tempo; rm -fr /var/*/salt" > /tmp/tempo.sh
